a8c8acf446418d370cdf63e215a0ca98f5128425
Gabrielle Wildfeuer <gpwildfeuer@email.wm.edu>
2020-02-13 21:27:58 +0100

    likes

--- frontend/chat/chat.jsx
+++ frontend/chat/chat.jsx
@@ -1,20 +1,5 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//   const chat = document.querySelector('#chat');
-
-//   for(const msg of conversation) {
-//     const name = msg.name.toLowerCase();
-//     chat.innerHTML += (
-//       `<div class="message message--${msg.side}">
-//         <img class="message__head" src="img/${name}.png"/>
-//         <div class="message__text">${msg.text}</div>
-//       </div>`
-//     );
-//   };
-// });
-
-
-
 import React from 'react';
+import ReactDOM from 'react-dom';
 import './chat.css';
 import unicorn_image from './img/unicorn.png';
 import cleverbot_image from './img/cleverbot.png';
@@ -22,35 +7,31 @@ import cleverbot_image from './img/cleverbot.png';
 
 export class Chat extends React.Component{
   state = {
-    button: "like"
+    likes : 0,
   }
 
-  addLike () {
-    if (this.state.button === "like"){
-      
+  addLike = () => {
+    this.setState({ 
+      likes : this.state.likes +=1
+    })
+  }
+
+  render () {
+    let name_img = unicorn_image;  
+    if (this.props.name === "Cleverbot"){
+      name_img = cleverbot_image
     }
-  }
 
-render () {
-  let name_img = unicorn_image;  
-  if (this.props.name === "Cleverbot"){
-    name_img = cleverbot_image
-  }
-
-  // let button = this.props.querySelector('.button');
-  // button.addEventListener('click', () => {
-  //   button.textContent = Number(button.textContent) + 1;
-  // })
-
-  return (
-        <> 
-          <div className={`message message--${this.props.side}`}>
-            <img className="message__head" src={name_img}/>
-            <div className="message__text">{this.props.text}<button className = "button">Like</button>
-              </div>
-          </div>
-        </>
-      )
-}
+    return (
+          <> 
+            <div className={`message message--${this.props.side}`}>
+              <img className="message__head" src={name_img}/>
+              <div className="message__text">{this.props.text}<button className = "button" onClick = {this.addLike}>Like</button>
+              <p class = "number">{this.state.likes}</p>
+                </div>
+            </div>
+          </>
+        )
+    }
 }
 
